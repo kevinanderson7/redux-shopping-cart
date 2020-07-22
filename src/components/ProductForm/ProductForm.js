d44ddc53ae7aa18e7b5cef6d594e34c5711683b1
Kevin Anderson <kmichael419@hotmail.com>
2020-07-22 15:31:54 -0500

    ProductForm dispatching to productReducer

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -1,46 +1,59 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ProductForm extends Component {
-    // You will need to keep this state in this component
-    // if you're only using something in one component,
-    // you do not need to move it to redux
-    state = {
-        productToAdd: {name: '', price: 0}
-    }
+  // You will need to keep this state in this component
+  // if you're only using something in one component,
+  // you do not need to move it to redux
+  state = {
+    productToAdd: { name: '', price: 0 },
+  };
 
-    handlePriceChange = (event) => {
-        this.setState({
-            productToAdd: {
-                ...this.state.productToAdd,
-                price: event.target.value
-            },
-        });
-    }
+  handlePriceChange = (event) => {
+    this.setState({
+      productToAdd: {
+        ...this.state.productToAdd,
+        price: Number(event.target.value),
+      },
+    });
+  };
 
-    handleNameChange = (event) => {
-        this.setState({
-            productToAdd: {
-                ...this.state.productToAdd,
-                name: event.target.value
-            },
-        });
-    }
+  handleNameChange = (event) => {
+    this.setState({
+      productToAdd: {
+        ...this.state.productToAdd,
+        name: event.target.value,
+      },
+    });
+  };
 
-    addProduct = (event) => {
-        event.preventDefault();
-        // TODO: Dispatch here
-        this.props.addNewProduct(this.state.productToAdd);
-    }
+  addProduct = (event) => {
+    event.preventDefault();
+    // TODO: Dispatch here
+    this.props.dispatch({
+      type: 'ADD_NEW_PRODUCT',
+      payload: this.state.productToAdd,
+    });
+    // this.props.addNewProduct(this.state.productToAdd);
+  };
 
-    render() {
-        return (
-            <form onSubmit={this.addProduct}>
-                <input onChange={this.handleNameChange} type="text" placeholder="name" />
-                <input onChange={this.handlePriceChange} type="text" placeholder="price" />
-                <input type="submit" value="Submit" />
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.addProduct}>
+        <input
+          onChange={this.handleNameChange}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          onChange={this.handlePriceChange}
+          type="text"
+          placeholder="price"
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-export default ProductForm;
+export default connect()(ProductForm);
