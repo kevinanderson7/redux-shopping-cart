95b94ebd98950889eafdc66a71e0c9fd443d9f31
Kevin Anderson <kmichael419@hotmail.com>
2020-07-22 16:40:19 -0500

    updating Product page to remove old state and data that is no longer needed with redux

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,27 +4,27 @@ import ProductList from '../ProductList/ProductList';
 
 class Products extends Component {
   // TODO: Use the productReducer instead of state
-  state = {
-    products: [
-      { name: 'Marshmallow Mateys', price: 6.98 },
-      { name: `Golden Honney O's`, price: 6.48 },
-      { name: `Frosted Flakes`, price: 3.98 },
-    ],
-  };
+  //   state = {
+  //     products: [
+  //       { name: 'Marshmallow Mateys', price: 6.98 },
+  //       { name: `Golden Honney O's`, price: 6.48 },
+  //       { name: `Frosted Flakes`, price: 3.98 },
+  //     ],
+  //   };
 
-  addNewProduct = (product) => {
-    console.log(product);
-    // TODO: Switch from using local state to dispatching an action
-    this.setState({
-      products: [...this.state.products, product],
-    });
-  };
+  //   addNewProduct = (product) => {
+  //     console.log(product);
+  //     // TODO: Switch from using local state to dispatching an action
+  //     this.setState({
+  //       products: [...this.state.products, product],
+  //     });
+  //   };
 
   render() {
     return (
       <div>
         <h2>Products</h2>
-        <ProductForm addNewProduct={this.addNewProduct} />
+        <ProductForm />
         <ProductList />
       </div>
     );
